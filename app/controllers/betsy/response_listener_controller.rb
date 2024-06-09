0e466e86611a9f74d4476947f90674a090d99e65
Dimitry Nazarov <dimitrynazarov@gmail.com>
2024-06-09 15:48:45 -0400

    replaces static confirmation view with a flash message and a redirect

--- app/controllers/betsy/response_listener_controller.rb
+++ app/controllers/betsy/response_listener_controller.rb
@@ -2,5 +2,7 @@ class Betsy::ResponseListenerController < ApplicationController
   def etsy_response_listener
     Betsy.request_access_token(params)
     Betsy.upsert_shop_id(params)
+
+    redirect_to Betsy.redirect_uri_base, allow_other_host: true, notice: "Etsy account connected successfully"
   end
 end
